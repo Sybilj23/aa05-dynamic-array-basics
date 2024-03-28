c6c33aa05d226e49ac913e1138c87e96e02b90ee
sybil <jordan.sybil@hotmail.com>
2024-03-28 00:38:22 -0700

    test

--- dynamic-array.js
+++ dynamic-array.js
@@ -1,22 +1,21 @@
 class DynamicArray {
-
-  constructor(defaultSize=4) {
-
-    // Your code here 
-
+  constructor(defaultSize = 4) {
+    this.data = new Array(defaultSize);
+    this.length = 0;
+    this.capacity = defaultSize;
   }
-
   read(index) {
-
-    // Your code here 
+    if (index >= this.length) return undefined;
+    return this.data[index];
   }
 
   unshift(val) {
-
-    // Your code here 
+    for(let i = this.length; i> 0; i--){
+      this.data[i] = this.data[i-1]
+    }
+    this.data[0] = val;
+    this.length++;
   }
-
 }
-
 
 module.exports = DynamicArray;
